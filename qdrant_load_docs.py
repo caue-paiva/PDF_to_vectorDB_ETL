d0cf525769afdb44b11de39889da7439204e086c
“Caue <cauepaivalira@outlook.com>
2024-01-01 16:31:37 -0300

    parte do langchain esta reconhecendo o conteudo e txt splitter ta funcionando

--- qdrant_load_docs.py
+++ qdrant_load_docs.py
@@ -1,7 +1,9 @@
-import os, qdrant_client
+import os, qdrant_client, re
+from types import new_class
 from typing import Iterable
 from openai import OpenAI
 from qdrant_client.models import Distance, VectorParams, PointStruct
+from qdrant_client.models import UpdateResult
 from dotenv import load_dotenv
 #código feito para carregar novos documentos na qdrant cloud
 load_dotenv(os.path.join("keys.env"))
@@ -12,32 +14,10 @@ embed_client = OpenAI()
 EMBEDDINGS_MODEL:str = 'text-embedding-ada-002'
 COLLECTION_NAME:str = os.getenv("QD_COLLECTION_NAME")
 OPENAI_VECTOR_PARAMS = VectorParams(size=1536, distance= Distance.COSINE, hnsw_config= None, quantization_config=None, on_disk=None)
+YEAR_PATTERN:str = "20\d{2}" #padrões REGEX para pegar o ano e a matéria de cada arquivo das questões
+SUBJECT_PATTERN: str = "_(.*?)_"
+CORRECT_ANSWER_STR: str = "(RESPOSTA CORRETA)"
 
-
-test_question:str = """(Enem/2020)  QUESTÃO 4          
-A Minor Bird
-I have wished a bird would fly away,
-And not sing by my house all day;
-Have clapped my hands at him from the door
-When it seemed as if I could bear no more.
-The fault must partly have been in me.
-The bird was not to blame for his key.
-And of course there must be something wrong
-In wanting to silence any song.
-FROST, R. West-running Brook. New York: Henry Holt and Company, 1928.
-No poema de Robert Frost, as palavras “fault” e “blame” 
-revelam por parte do eu lírico uma
-A)culpa por não poder cuidar do pássaro. 
-B)atitude errada por querer matar o pássaro. 
-C)necessidade de entender o silêncio do pássaro. 
-D)sensibilização com relação à natureza do pássaro. 
-E)irritação quanto à persistência do canto do pássaro.
-
-(RESPOSTA CORRETA): D"""
-
-query_test:str = """
-me de uma questao sobre No poema de Robert Frost, as palavras “fault” e “blame”
-"""
 
 client2 = qdrant_client.QdrantClient(
      url=os.getenv("QDRANT_HOST"),
@@ -50,21 +30,28 @@ def qdrant_recreate_collection(name:str, QDclient: qdrant_client.QdrantClient, p
         vectors_config=parameters
     )
  
-def upsert_func(docs:list[list], QDclient: qdrant_client.QdrantClient)->None:
-    print("upsert")
+"""def QD_upsert_func(text:str, 
+    vectors:list[list], 
+    QDclient: qdrant_client.QdrantClient, 
+    start_id: int ,
+    question_year:int,
+    question_subject:str,   
+    target_collection: str        
+)->None:
+    
     QDclient.upsert(
-    collection_name= COLLECTION_NAME,
+    collection_name= target_collection,
     points= [
         PointStruct(
             id = idx,
             vector = vector,
-            payload= {"texto":test_question, "materia": "eng", "ano": 2020}
+                payload= {"page_content":text, "metadata": {"materia": question_subject, "ano": question_year}}
         )
-        for idx, vector in enumerate(docs,0)
+        for idx, vector in enumerate(vectors,start_id)
     ]
-    )
+    )"""
 
-def add_func(in_docs:list, in_metadata:dict, vector_count:int = 1)->None:
+def QDadd_func(in_docs:list, in_metadata:dict, vector_count:int = 1)->None:
   print("add")
   in_docs = [in_docs]
   in_metadata = [in_metadata]
@@ -82,16 +69,18 @@ def get_openAI_embeddings(text:str)->list[float]:
     )
     return response.data[0].embedding
 
-def vector_search(vector:list[float], QDclient)->list:
+def QDvector_search(vector:list[float], QDclient :qdrant_client.QdrantClient, target_collection:str , vector_num:int = 1)->list:
    search = QDclient.search(
-    collection_name=COLLECTION_NAME,
+    collection_name=target_collection,
     query_vector = vector,
-    limit=1
+    limit= vector_num
    )
-
    return search
 
 def text_chunk_splitter(text:str, split_key:str)->Iterable[str]: #a split key vai ser (RESPOSTA CORRETA)
+   """
+   Iterador que retorna a proxima substr que corresponde a uma questão inteira
+   """
    current_key_posi: int = 0
    CORRECT_ALTERNATIVE_BUFF: int = 22  #tamanho da split key até a alternativa correta:  (RESPOSTA CORRETA): D
    
@@ -100,28 +89,87 @@ def text_chunk_splitter(text:str, split_key:str)->Iterable[str]: #a split key va
       current_key_posi = next_key_posi + CORRECT_ALTERNATIVE_BUFF  #começar a procurar da posição atual + buffer
       yield str_slice
 
+def text_to_vectorDB(txt_file_path:str, QDclient:qdrant_client.QdrantClient, target_collection:str)->None:
+  
+    if ".txt" not in txt_file_path:
+       raise IOError("essa função apenas aceita arquivos .TXT")
 
-get_payload = lambda x : x[0].payload
+    if not isinstance(txt_file_path, str):
+        raise TypeError("path para o arquivo não é uma string")
 
-collection:object = client2.get_collection(COLLECTION_NAME)
-vector_count:int = collection.vectors_count
+    if not isinstance(QDclient,qdrant_client.QdrantClient):
+        raise TypeError("Argumento do cliente do Qdrant não é do tipo suportadp")
+ 
+    if not (matches_list_year := re.findall(YEAR_PATTERN,txt_file_path)):
+       raise IOError("Nome do arquivo não tem referencia ao ano da prova")
+    else:
+       test_year: int = int(matches_list_year[0])
+    
+    if not (matches_list_subj := re.findall(SUBJECT_PATTERN, txt_file_path)):
+       raise IOError("Nome do arquivo não tem referencia à matéria das questões")
+    else:  
+        subject:str = matches_list_subj[0]
 
-print(vector_count)
-print(f"my collection:  {collection}")
+    collection:object = QDclient.get_collection(target_collection)
+    print(collection)
+    vector_count:int = collection.vectors_count     #esse vai ser o id que o vetor a ser inserido vai ter, ele depende da quantidade de vetores já existentes
+    start_amount: int = vector_count  #o primeiro vetor foi add com id=0 , o segundo com id=1, então o ID do novo vai ser a qntd de vetores ja existentes
+    
+    with open(txt_file_path, "r") as f:
+        entire_text: str = f.read()
+
+    for text_chunk in text_chunk_splitter(entire_text, CORRECT_ANSWER_STR):  #
+        vectors:list[list[float]] =  [get_openAI_embeddings(text_chunk)]
+        
+        QDclient.upsert(
+            collection_name= target_collection,
+            points= [
+             PointStruct(
+               id = idx,
+               vector = vector,
+               payload= {"page_content":text_chunk, "metadata": {"materia": subject, "ano": test_year}}
+             )
+            for idx, vector in enumerate(vectors,vector_count)] 
+        )
+        vector_count += 1
+    
+    all_new_questions: int = vector_count - start_amount
+    print(f"Tentou inserir {all_new_questions} questões no vectorDB") #quantidade de novas questões
+
+    final_vector_count: int  = QDclient.get_collection(target_collection).vectors_count #mudança na qntd de vetores no vector db
+    questions_added: int = final_vector_count - start_amount
+    print(f"Foram inseridas  {questions_added} questões no vectorDB")     
+
+    if all_new_questions !=  questions_added:
+       print("Não foi possível adicional todas as questões no vectorDB") 
+
+#text_to_vectorDB("2020_D1_/2020_spani_questions.txt", client2, COLLECTION_NAME)
+
+print(QDvector_search(
+   vector= get_openAI_embeddings("refugiado nigeriano"),
+   QDclient= client2,
+   target_collection=COLLECTION_NAME
+))
+
+
+#get_payload = lambda x : x[0].payload
+#collection:object = client2.get_collection(COLLECTION_NAME)
+#vector_count:int = collection.vectors_count
+#print(vector_count)
+#print(f"my collection:  {collection}")
 #metadata = {"materia": "eng", "ano": 2020}
 
-upsert_func([get_openAI_embeddings(test_question)], client2)
-
+#upsert_func(test_question, [get_openAI_embeddings(test_question)], client2,vector_count)
+#collection:object = client2.get_collection(COLLECTION_NAME)
 
 #antes da primeira adição tinhamos 44 vectors na collection
 
 #print(client2.get_collection(COLLECTION_NAME))
 
-print(type(vector_search(get_openAI_embeddings(query_test),client2)))
-
-print("\n\n\n")
-
 #print(client.get_collection(COLLECTION_NAME))
+
+#extract_text("2020_D1_/2020_huma_questions.txt")
+
 
 
 """
